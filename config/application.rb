996d6ae9f7aa11ed570a074c045314db2b973515
Tyler Gannon <tgannon@gmail.com>
2017-08-22 19:50:37 +0530

    Also want these

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,9 +18,6 @@ Bundler.require(*Rails.groups)
 
 module Polestar
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
