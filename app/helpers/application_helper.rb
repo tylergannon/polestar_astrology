4d43e43d5f1bf2cd020e327f1204111522fb3ea3
Tyler Gannon <tgannon@gmail.com>
2017-08-24 17:26:13 +0530

    A couple more style fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,11 @@ module ApplicationHelper
     @browser.device.mobile?
   end
 
+  def hella_old?
+    @browser ||= Browser.new(request.user_agent, accept_language: "en-us")
+    @browser.chrome? && browser.version.to_i <= 100
+  end
+
   def months
     %w(January Fabruary March April May June July August September October November December).zip((1..12).to_a)
   end
