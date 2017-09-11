79edcfd1aaeeb9e1793c673b5fc840d551f0e786
Tyler Gannon <tgannon@gmail.com>
2017-09-11 13:34:55 +0000

    Migrate over to Vagrant dev environment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170822092336) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alternate_emails", id: :serial, force: :cascade do |t|
     t.string "email"
     t.integer "member_id"
